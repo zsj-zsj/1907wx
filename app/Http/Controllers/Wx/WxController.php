794631dc85228c5031905ed19a54eaf66d35b443
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-01-13 14:04:27 +0800

    s

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -276,13 +276,15 @@ class WxController extends Controller
       $redirect_uri=urlEncode('http://www.zsjshaojie.top/auth');
       $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('APPID').'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
       // dd($url);
-     
       echo $url;
     }
 
     public function auth(){
       $code=$_GET['code'];
-
+      $url=' https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('APPSECRET').'&code='.$code.'&grant_type=authorization_code';
+      $json=file_get_contents($url);
+      $data=json_decode($json,true);
+      dd($data);
     }
 
 
