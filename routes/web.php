a71f84773af80787b7db0c6beb378457ca4159f7
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-01-13 08:51:06 +0800

    s

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,10 @@ Route::post('wx','Wx\WxController@wxdo');
 Route::get('menu','Wx\WxController@menu');    //菜单
 Route::any('gitpull','Wx\WxController@gilPull');    //git自动拉取
 
+
 Route::get('downloadImg','Wx\WxController@downloadImg');    //保存用户发过来的消息
 Route::get('semdAllOpenid','Wx\WxController@semdAllOpenid');    //保存用户发过来的消息
+
 
 //后台
 Route::get('login','Login\Login@login'); //登录页面
