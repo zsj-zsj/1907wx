acebd755f4b5b7d50e99fb92449281148e22bb97
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-01-13 14:18:24 +0800

    s

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -281,7 +281,7 @@ class WxController extends Controller
 
     public function auth(){
       $code=$_GET['code'];
-      $url=' https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('APPSECRET').'&code='.$code.'&grant_type=authorization_code';
+      $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('APPSECRET').'&code='.$code.'&grant_type=authorization_code';
       $json=file_get_contents($url);
       $data=json_decode($json,true);
       dd($data);
