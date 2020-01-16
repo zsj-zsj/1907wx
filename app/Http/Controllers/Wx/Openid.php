f1817e9ad9267c41eeb7fa838637fddd9cb651e9
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-01-16 11:34:51 +0800

    s

--- app/Http/Controllers/Wx/Openid.php
+++ app/Http/Controllers/Wx/Openid.php
@@ -15,9 +15,15 @@ class Openid extends Controller
 
     public function code(){
         $code=$_GET['code'];
-        dd($code);
+        // dd($code);
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('APPSECRET').'&code='.$code.'&grant_type=authorization_code';
-        dd($url);
+        // dd($url);
+        $json=file_get_contents($url);
+        $data=json_decode($json,true);
+        print_r($data);
+
+
+
         return view('admin/openid/login');
     }
 }
