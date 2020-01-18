f075fc447f6182c473bbae7460bd3f2f1aa5aca5
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-01-18 17:37:09 +0800

    g

--- app/Http/Controllers/Wx/Openid.php
+++ app/Http/Controllers/Wx/Openid.php
@@ -71,15 +71,15 @@ class Openid extends Controller
         $url="http://www.zsjshaojie.top/openid/sscan?status=".$status;
         $redirect_uri=urlEncode('http://www.zsjshaojie.top/openid/sscan');
         $urls='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('APPID').'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-        return view('admin/login/ewm',['url'=>$url,'status'=>$status]);
+        return view('admin/login/ewm',['url'=>$url,'status'=>$status,'urls'=>$urls]);
     }
 
 
     public function sscan(){
         $id=request('status');
-        $redirect_uri=urlEncode('http://www.zsjshaojie.top/openid/sscan');
-      $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('APPID').'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-        echo $url;
+    //     $redirect_uri=urlEncode('http://www.zsjshaojie.top/openid/sscan');
+    //   $url='https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('APPID').'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+    //     echo $url;
       $code=$_GET['code'];
 
       $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('APPSECRET').'&code='.$code.'&grant_type=authorization_code';
