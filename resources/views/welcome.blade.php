a3ca4ce95388db67944988026ce0402c3df77b0d
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-02-10 14:59:34 +0800

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravellaravel
                 </div>
 
                 <div class="links">
